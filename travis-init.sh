fd168b95ed75a480a7ce062899d2950bab491922
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-05-04 23:17:23 +0200

    eio seems to be added to php.ini by pecl

--- travis-init.sh
+++ travis-init.sh
@@ -7,7 +7,7 @@ if [[ "$TRAVIS_PHP_VERSION" != "hhvm" ]]; then
     # install 'eio' PHP extension (does not support php 7)
     if [[ "$TRAVIS_PHP_VERSION" != "7.0" ]]; then
         yes "" | pecl install eio
-        echo "extension=eio.so" >> "$(php -r 'echo php_ini_loaded_file();')"
+#        echo "extension=eio.so" >> "$(php -r 'echo php_ini_loaded_file();')"
     fi
 
 fi
