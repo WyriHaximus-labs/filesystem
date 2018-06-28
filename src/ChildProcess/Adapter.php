5786f0fd18a9af61345e65de9bda34fe38c2d265
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-06-28 08:10:02 +0200

    Translate mode from string to numbers before passing it on to WoolTrait

--- src/ChildProcess/Adapter.php
+++ src/ChildProcess/Adapter.php
@@ -176,7 +176,7 @@ class Adapter extends AbstractSyncAdapter implements AdapterInterface
             return $this->fileDescriptors[$id]->rpc(Factory::rpc('open', [
                 'path' => $path,
                 'flags' => $flags,
-                'mode' => $mode,
+                'mode' => decoct($this->permissionFlagResolver->resolve($mode)),
             ]));
         })->then(function () use ($path, $flags, &$id) {
             return \React\Promise\resolve(StreamFactory::create($path, $id, $flags, $this));
